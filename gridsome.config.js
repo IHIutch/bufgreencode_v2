3cb9308f5cf4c89124171259ea04eb21775d47cd
IHIutch <jbhutch01@gmail.com>
2019-09-07 12:41:30 -0400

    Styling, tailwind, purgecss setup

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,34 @@
-// This is where project configuration and plugin options are located. 
+// This is where project configuration and plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
+const tailwind = require("tailwindcss");
+const autoprefixer = require("autoprefixer");
+const purgecss = require("@fullhuman/postcss-purgecss");
+
+if (process.env.NODE_ENV === "production") postcssPlugins.push(purgecss());
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
-}
+  siteName: "Gridsome",
+  transformers: {
+    remark: {}
+  },
+  plugins: [
+    {
+      use: "@gridsome/source-filesystem",
+      options: {
+        path: "content/**/*.md",
+        typeName: "Post",
+        route: "/content/sections/:article/:slug"
+      }
+    }
+  ],
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [tailwind(), autoprefixer()]
+      }
+    }
+  }
+};
---
+++ postcss.config.js
@@ -0,0 +1,33 @@
+const purgecss = require("@fullhuman/postcss-purgecss");
+
+module.exports = {
+  plugins: [
+    purgecss({
+      content: ["src/**/*.vue"],
+      whitelist: [
+        "body",
+        "html",
+        "img",
+        "a",
+        "g-image",
+        "g-image--lazy",
+        "g-image--loaded",
+        "h1",
+        "h2",
+        "h3",
+        "h4",
+        "h5",
+        "h6",
+        "table",
+        "tr",
+        "td",
+        "th",
+        "tbody",
+        "thead",
+        "ul",
+        "ol",
+        "li"
+      ]
+    })
+  ]
+};
