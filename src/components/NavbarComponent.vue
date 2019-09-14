3ece383a98ad07ffdb56b242e1b8694a146e6357
IHIutch <jbhutch01@gmail.com>
2019-09-14 19:20:23 -0400

    Update wrapper logo in nav

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,17 +1,18 @@
 <template>
   <nav
-    class="flex bg-white border-b border-gray-200 fixed top-0 z-100 h-16 inset-x-0"
+    class="flex bg-white border-b border-gray-200 fixed top-0 z-10 h-16 inset-x-0"
   >
     <div class="w-full max-w-screen-xl relative mx-auto px-6">
-      <div class="flex items-center -mx-6">
-        <div class="lg:w-1/4 xl:w-1/5 pl-6 pr-6 lg:pr-8">
-          <div class="flex items-center">
-            <g-link to="/" class="block lg:mr-4"
-              ><g-image
+      <div class="flex items-center -mx-6 h-full">
+        <div class="lg:w-1/4 xl:w-1/5 pl-6 pr-6 lg:pr-8 h-full">
+          <div class="flex items-center h-full">
+            <g-link to="/" class="block lg:mr-4 h-full"
+              ><img
                 src="../../assets/images/meta/meta-img.png"
                 alt="Buffalo Green Code"
-              ></g-image
-            ></g-link>
+                class="h-full w-auto"
+              />
+            </g-link>
           </div>
         </div>
       </div>
