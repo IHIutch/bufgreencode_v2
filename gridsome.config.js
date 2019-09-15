6d5ebe34cd36ce33fd0a2df6848b26d22b61095c
IHIutch <jbhutch01@gmail.com>
2019-09-15 13:15:34 -0400

    Update config, add favicon

--- gridsome.config.js
+++ gridsome.config.js
@@ -21,36 +21,30 @@ module.exports = {
       use: "@gridsome/source-filesystem",
       options: {
         typeName: "Post",
-        path: "content/**/*.md",
+        path: "./content/**/*.md",
         route: "/:article/:slug"
       }
     },
     {
       use: "@gridsome/plugin-sitemap"
     },
-    // {
-    //   use: "@gridsome/plugin-google-analytics",
-    //   options: {
-    //     id: process.env.GOOGLE_ANALYTICS
-    //   }
-    // },
-    // {
-    //   use: "gridsome-plugin-gtm",
-    //   options: {
-    //     id: process.env.GOOGLE_TAG_MANAGER,
-    //     enabled: true,
-    //     debug: true
-    //   }
-    // },
-    // {
-    //   use: "gridsome-plugin-algolia",
-    //   options: {
-    //     appId: process.env.ALGOLIA_APP_ID,
-    //     apiKey: process.env.ALGOLIA_ADMIN_KEY,
-    //     collections,
-    //     enablePartialUpdates: true // default: false
-    //   }
-    // },
+    {
+      use: "gridsome-plugin-gtm",
+      options: {
+        id: process.env.GOOGLE_TAG_MANAGER,
+        enabled: true,
+        debug: true
+      }
+    },
+    {
+      use: "gridsome-plugin-algolia",
+      options: {
+        appId: process.env.ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        collections,
+        enablePartialUpdates: true // default: false
+      }
+    },
     {
       use: "gridsome-plugin-pwa",
       options: {
@@ -63,7 +57,7 @@ module.exports = {
         shortName: "Gridsome",
         themeColor: "#666600",
         backgroundColor: "#ffffff",
-        icon: "" // must be supplied!
+        icon: "./assets/images/meta/favicon.png" // must be supplied!
       }
     }
   ],
