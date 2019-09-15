14e06b7bca595579a9bfd75bd9dde0209ab4a5a0
IHIutch <jbhutch01@gmail.com>
2019-09-15 17:10:59 -0400

    Try to add tag manager with vue-gtm

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 import "~/assets/scss/main.scss";
+import VueGtm from "vue-gtm";
+
+export default function(Vue, { router, head, appOptions, isClient }) {
+  if (isClient) {
+    Vue.use(VueGtm, {
+      vueRouter: router,
+      id: process.env.GOOGLE_TAG_MANAGER,
+      enabled: true,
+      debug: false
+    });
+  }
+}
