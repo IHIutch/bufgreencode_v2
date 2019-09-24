dea534a92e0f39e617c106338828f561c65ad2f8
IHIutch <jbhutch01@gmail.com>
2019-09-23 22:59:23 -0400

    Meta updates

--- src/main.js
+++ src/main.js
@@ -4,14 +4,40 @@ import "./assets/scss/main.scss";
 
 import DefaultLayout from "~/layouts/Default.vue";
 
+const siteUrl = "https://gridsome.bufgreencode.com/";
+const siteName = "Buffalo Green Code";
+const metaImage = siteUrl + "meta/meta-img.png";
+
 export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component("Layout", DefaultLayout);
-
   // Add a meta tag
   head.meta.push({
+    property: "og:type",
+    href: "article"
+  });
+  head.meta.push({
+    property: "og:url",
+    href: siteUrl
+  });
+  head.meta.push({
+    property: "og:site_name",
+    href: siteName
+  });
+  head.meta.push({
     property: "og:image",
-    href: "./meta/meta-img.png",
-    content: "./meta/meta-img.png"
+    href: metaImage
+  });
+  head.meta.push({
+    name: "twitter:card",
+    href: "summary"
+  });
+  head.meta.push({
+    name: "twitter:image",
+    href: metaImage
+  });
+  head.meta.push({
+    name: "twitter:author",
+    href: "@jb_hutch"
   });
 }
