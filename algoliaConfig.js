2436f66cd766b665be7e138208cb73ee637b807b
IHIutch <jbhutch01@gmail.com>
2019-09-26 20:55:01 -0400

    Update algolla config

--- algoliaConfig.js
+++ algoliaConfig.js
@@ -1,27 +1,30 @@
 [
   {
     query: `{
-      allBlogPost {
+      allPost {
         edges {
-          node {
-            id
-            title
-            slug
-            modified
-          }
+            node {
+                id
+                title
+                path
+                content
+                headings{depth, value}
+                }
+            }
         }
-      }
     }`,
-    transformer: ({ data }) => data.allBlogPost.edges.map(({ node }) => node),
-    indexName: process.env.ALGOLIA_INDEX, // Algolia index name
+    transformer: ({ data }) => data.allPost.edges.map(({ node }) => node),
+    indexName: process.env.ALGOLIA_INDEX,
     itemFormatter: item => {
       return {
         objectID: item.id,
         title: item.title,
         slug: item.slug,
-        modified: String(item.modified)
+        path: item.path,
+        content: item.content,
+        type: "document"
       };
-    }, // optional
-    matchFields: ["slug", "modified"] // Array<String> required with PartialUpdates
+    },
+    matchFields: ["slug"] // Array<String> required with PartialUpdates
   }
 ];
