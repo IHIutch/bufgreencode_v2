7ec5a1996c60195bbd7e399407e5706431914bd9
Hutchison <jhutchison@CTD24427JHUTCH.local>
2022-03-08 13:27:56 -0500

    Different fathom steup

--- gridsome.config.js
+++ gridsome.config.js
@@ -36,14 +36,6 @@ module.exports = {
       use: "@gridsome/plugin-sitemap",
     },
     {
-      use: "gridsome-plugin-fathom",
-      options: {
-        siteId: "HWWXLVYL",
-        host: "phenomenal-bright.bufgreencode.com",
-        debug: false,
-      },
-    },
-    {
       use: "gridsome-plugin-gtm",
       options: {
         id: "GTM-KNPMJGD",
