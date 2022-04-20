addc73b67fa717e9d30f1cdaf9bb07b0f2ce121a
JB Hutch <jbhutch01@gmail.com>
2022-04-19 23:18:11 -0400

    Install cssnano, generate css map

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,12 @@
-module.exports = {
+module.exports = (ctx) => ({
+  map: ctx.options.map,
   plugins: {
     'postcss-import': {},
     'tailwindcss/nesting': {},
     tailwindcss: {},
     autoprefixer: {},
+    cssnano: {
+      preset: 'default',
+    },
   },
-}
+})
