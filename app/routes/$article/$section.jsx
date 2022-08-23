1ae7b13ed42a58f1dd86e380616f262318d70ee4
JB Hutch <jbhutch01@gmail.com>
2022-08-22 21:44:47 -0400

    Fix tooltip

--- app/routes/$article/$section.jsx
+++ app/routes/$article/$section.jsx
@@ -64,29 +64,31 @@ const Heading = ({ id, level, children }) => {
 
   return (
     <div className="mt-[2em]">
-      <Tooltip.Root open={isToolTipVisible}>
-        <Tooltip.Trigger asChild>
-          <button
-            type="button"
-            className="mb-1 flex items-center text-sm font-semibold text-green-600 underline transition-colors hover:text-green-700"
-            onClick={copyLinkToClipboard}
+      <Tooltip.Provider>
+        <Tooltip.Root open={isToolTipVisible}>
+          <Tooltip.Trigger asChild>
+            <button
+              type="button"
+              className="mb-1 flex items-center text-sm font-semibold text-green-600 underline transition-colors hover:text-green-700"
+              onClick={copyLinkToClipboard}
+            >
+              <LinkIcon strokeWidth="3" className="h-[0.875rem] w-[0.875rem]" />
+              <div>
+                <span className="ml-1">Share Section</span>
+              </div>
+            </button>
+          </Tooltip.Trigger>
+          <Tooltip.Content
+            className={clsx(
+              'rdx-tooltip',
+              'inline-flex items-center rounded-md px-2 py-1',
+              'bg-gray-800 text-xs text-white'
+            )}
           >
-            <LinkIcon strokeWidth="3" className="h-[0.875rem] w-[0.875rem]" />
-            <div>
-              <span className="ml-1">Share Section</span>
-            </div>
-          </button>
-        </Tooltip.Trigger>
-        <Tooltip.Content
-          className={clsx(
-            'rdx-tooltip',
-            'inline-flex items-center rounded-md px-2 py-1',
-            'bg-gray-800 text-xs text-white'
-          )}
-        >
-          Copied to clipboard
-        </Tooltip.Content>
-      </Tooltip.Root>
+            Copied to clipboard
+          </Tooltip.Content>
+        </Tooltip.Root>
+      </Tooltip.Provider>
       {headingEl}
     </div>
   )
