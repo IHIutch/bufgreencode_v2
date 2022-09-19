4345623674601de665dc1020f9bb76b98852f034
JB Hutch <jbhutch01@gmail.com>
2022-09-19 12:20:30 -0400

    Update fathom goal tracking

--- app/routes/$article/$section.jsx
+++ app/routes/$article/$section.jsx
@@ -57,7 +57,9 @@ const Heading = ({ id, level, children }) => {
     )
 
     if (process.env.NODE_ENV === 'production') {
-      window.fathom.trackGoal('RC2RYOND', 0)
+      window.fathom.trackGoal('RC2RYOND', {
+        anchor: '#' + id,
+      })
     }
     setTimeout(() => {
       setIsToolTipVisible(false)
