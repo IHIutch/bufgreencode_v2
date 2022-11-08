ec493f3e3007edf23903d73bd8941e8ed719a589
JB Hutch <jbhutch01@gmail.com>
2022-11-08 17:12:18 -0500

    Add vercel web vitals manually

--- app/entry.client.jsx
+++ app/entry.client.jsx
@@ -1,4 +1,8 @@
 import { RemixBrowser } from '@remix-run/react'
 import { hydrate } from 'react-dom'
+import reportWebVitals from './reportWebVitals'
+import { sendToVercelAnalytics } from './vitals'
 
 hydrate(<RemixBrowser />, document)
+
+reportWebVitals(sendToVercelAnalytics)
