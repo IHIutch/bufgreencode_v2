51c009ab2a2285199ba59328caad3617c5d19969
JB Hutch <jbhutch01@gmail.com>
2023-02-23 17:35:22 -0500

    Update content components

--- src/components/TableSmall.tsx
+++
@@ -1,9 +0,0 @@
-import type { ReactElement } from 'react'
-
-export default function TableSmall({ children }: { children: ReactElement }) {
-  return (
-    <div className="table-bordered table-hidden-thead mb-4 -mt-2">
-      {children}
-    </div>
-  )
-}
