855b38715d84f40ec220547eaf48826a0b33f56e
JB Hutch <jbhutch01@gmail.com>
2023-02-24 09:49:38 -0500

    Fix scroll spy and scroll margins

--- src/hooks/useScrollSpy.ts
+++ src/hooks/useScrollSpy.ts
@@ -1,47 +1,18 @@
 import { useState, useRef, useEffect } from 'react'
 
-export function useScrollSpy(selectors: string[]) {
-  console.log({ selectors })
-
+export function useScrollSpy(selectors: Selector[]) {
   const [activeId, setActiveId] = useState<string | null>()
-  const observer = useRef()
+  const observer = useRef(null)
   useEffect(() => {
     const elements = selectors.map((selector) =>
       document.querySelector(selector)
     )
-    // observer?.current?.disconnect()
+    observer.current?.disconnect()
     observer.current = new IntersectionObserver(
       (entries) => {
-        entries.forEach((entry, idx) => {
-          // console.log({
-          //   //   target: entry.target,
-          //   entry,
-          // })
-          if (
-            entry.boundingClientRect.top < entry.intersectionRect.top &&
-            entry.intersectionRatio > 0
-          ) {
-            if (entry.isIntersecting) {
-              console.log('active from top', entry.target)
-              setActiveId(entry.target.getAttribute('id'))
-            } else {
-              // console.log('inactive from top')
-              // entries[idx + 1] &&
-              //   setActiveId(entries[idx + 1].target.getAttribute('id'))
-            }
-            // console.log({ target: entry.target, entry }, 'top')
-          } else if (
-            entry.boundingClientRect.bottom > entry.intersectionRect.bottom &&
-            entry.intersectionRatio > 0
-          ) {
-            if (entry.isIntersecting) {
-              console.log('active from bottom', entry.target)
-              setActiveId(entry.target.getAttribute('id'))
-            } else {
-              // console.log('inactive from bottom')
-              // entries[idx - 1] &&
-              //   setActiveId(entries[idx - 1].target.getAttribute('id'))
-            }
+        entries.forEach((entry) => {
+          if (entry?.isIntersecting) {
+            setActiveId(entry.target.getAttribute('id'))
           }
         })
       },
