9fc44694fb81d2e3aa3479e611d83edccd11b8e9
JB Hutch <jbhutch01@gmail.com>
2023-08-28 10:55:39 -0400

    Fix robots.txt

--- src/app/robots.tsx
+++ src/app/robots.tsx
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: 'hhttps://bufgreencode.com/sitemap.xml',
+    sitemap: 'https://bufgreencode.com/sitemap.xml',
   }
 }
